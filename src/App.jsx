92c8410ad6b4846bb6a0edfe3c13418bb13a2cc9
Shubhi989 <shubhangiagarwal989@gmail.com>
2026-05-27 21:47:05 +0530

    icons and images from figma added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ const App = () => {
    <main>
     <div className="pattern"/>
     <div className="wrapper">
-      
+      <header>
+        <h1>
+          Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle
+        </h1>
+      </header>
     </div>
 
    </main>
